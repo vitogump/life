60b7911f8a99c6b57dd37209f18713fb3fe0eb97
liurui <vitogump@163.com>
2014-11-10 13:12:31 +0800

    func almost finished

--- src/pipelinecontrol/Util.py
+++ src/pipelinecontrol/Util.py
@@ -4,81 +4,110 @@ Created on 2014-11-8
 
 @author: liurui
 '''
-import os,re
+import os, re
 
 class OperatorWithData():
-    def __init__(self):
-        pass
-    def process(self,p,d):
-        print(p,d)
-#myprint=OperatorWithData()
+    def __init__(self, scriptsdir="F:/work/pipelinecontrol/scripts"):
+        self.scriptsdir = scriptsdir + "/"
+    def process(self, p, d):
+        print(p, d)
+# myprint=OperatorWithData()
 class OperatorWithData_mode1(OperatorWithData):
-    def __init__(self,cmdline):
+    def __init__(self, cmdline, outputpath, suffix, inputdatapath, n_subdirs):
         super().__init__()
-        self.cmdline=cmdline
-    def process(self,curpath,curdepth):
+        self.cmdline = cmdline
+        self.outputpath = outputpath
+        self.suffix = suffix
+        self.inputdatapath = inputdatapath
+        self.n_subdirs = n_subdirs
+    def process(self, curpath, curdepth):
         print("mode1 process")
-        newcmdline=self.cmdline
-        subtargets=re.findall(r"\${.*?}",newcmdline)
-        targetdatasuffix=[]
+        newcmdline = self.cmdline
+        subtargets = re.findall(r"\${.*?}", newcmdline)
+        targetdatasuffix = []
         for target in subtargets:
-            c=re.search(r'\${(.*?)}',target).group(1)
-            if "output"==c:
-                print(target,subtargets)
+            c = re.search(r'\${(.*?)}', target).group(1)
+            if "output" == c:
+                print(target, subtargets)
                 subtargets.remove(target)
                 continue
             targetdatasuffix.append(c)
         
-        datafiles=os.listdir(path=curpath)
-        print("OperatorWithData_mode1",datafiles)
-        for i in range(0,len(targetdatasuffix)):
+        datafiles = os.listdir(path=curpath)
+        updirname = re.search(r".*/([^/]+)$", curpath).group(1)
+        print("OperatorWithData_mode1", datafiles)
+        pathToOutputdata_createdir = ""
+        if self.inputdatapath != self.outputpath and self.n_subdirs <= curdepth:
+            pathToOutputdata_createdir = re.search(r"" + self.inputdatapath + "((/.*?){" + str(self.n_subdirs) + "}[/])", curpath + "/").group(1)
+            leftPathName_filenamepre = re.search(r"" + self.inputdatapath + pathToOutputdata_createdir + "(.*)", curpath + "/").group(1).replace("/", ".")
+            if not os.path.exists(self.outputpath + pathToOutputdata_createdir):
+                os.makedirs(self.outputpath + pathToOutputdata_createdir)
+        elif self.n_subdirs > curdepth:
+            print(curdepth, self.n_subdirs, "OperatorWithData_mode1 error")
+            exit(-1)
+        for i in range(0, len(targetdatasuffix)):
             for datafilename in datafiles:
-                if re.search(r".*?"+targetdatasuffix[i],datafilename)!=None:
-                    newcmdline=re.sub(r"\${\s*"+targetdatasuffix[i]+"\s*}"," "+curpath+"/"+datafilename+" ",newcmdline)
+                if re.search(r".*?" + targetdatasuffix[i], datafilename) != None:
+                    newcmdline = re.sub(r"\${\s*" + targetdatasuffix[i] + "\s*}", " " + curpath + "/" + datafilename + " ", newcmdline)
+                    if self.inputdatapath == self.outputpath:  # input data files and output data files are in the same dir. and this situation leftPathName_filenamepre==None 
+                        newcmdline = re.sub(r"\${output}", self.outputpath + "/" + updirname + "." + self.suffix, newcmdline)
+                    else:  # when curdepth ==  self.n_subdirs,the leftPathName_filenamepre contain the updirname
+                        newcmdline = re.sub(r"\${output}", self.outputpath + pathToOutputdata_createdir + leftPathName_filenamepre + updirname + "." + self.suffix, newcmdline)
+                    
+                        
                     # sub was acted from the first to the rear most
+        print(newcmdline, file=open(self.scriptsdir + pathToOutputdata_createdir.replace("/", ".")[1:] + updirname + "_script.sh", "a"))
         return newcmdline
 class OperatorWithData_mode2(OperatorWithData):
-    def __init__(self,cmdline):
+    def __init__(self, cmdline, outputpath, suffix):
         super().__init__()
-        self.cmdline=cmdline
-        self.newcmdline=""
-    def process(self,curpath,curdepth):
+        optionstr = re.search(r"([-\w\d]+[=\s]+)\${output}", cmdline).group(1)  # for example "INPUT=${.bam} -i ${.sam}"
+
+        self.newcmdline = re.sub(r"[-\w\d]+[=\s]+\${output}",optionstr +outputpath+"/"+ suffix  + " ",cmdline)
+        print("OperatorWithData_mode2 __init__",self.newcmdline)
+#         self.outputpath = outputpath
+#         self.suffix = suffix
+    def process(self, curpath, curdepth):
         print("mode2 process")
-        newcmdline=self.cmdline
+        newcmdline = self.newcmdline
 
-        option_suffix_obj=re.search(r"(-\wd+)\s+\${(.*?)}",newcmdline)# for example "-INPUT ${.bam}"
-        optionstr=option_suffix_obj.group(1)
-        suffixstr=option_suffix_obj.group(2)
+        option_suffix_obj = re.search(r"([-\w\d]+[=\s]+)\${(.*?)}", newcmdline)  # for example "INPUT=${.bam} -i ${.sam}"
+        optionstr = option_suffix_obj.group(1)
+        suffixstr = option_suffix_obj.group(2)
+        print(optionstr, suffixstr)
         
-        datafiles=os.listdir(path=curpath)
-        print("OperatorWithData_mode2",datafiles)
+        datafiles = os.listdir(path=curpath)
+        print("OperatorWithData_mode2", datafiles)
         for datafilename in datafiles:
-            if re.search(r".*?"+suffixstr,datafilename)!=None:
-                newcmdline=re.sub(r"-\wd+\s+\${.*?}",curpath+"/"+datafilename+" "+option_suffix_obj.group(0),newcmdline)
+#             print("curfilename", curpath + "/" + datafilename)
+            if re.search(r".*?" + suffixstr, datafilename) != None:
+                newcmdline = re.sub(r"[-\w\d]+[=\s]+\${.*?}", optionstr + " " + curpath + "/" + datafilename + " " + option_suffix_obj.group(0), newcmdline)
+#                 print(newcmdline)
 
-        self.newcmdline=newcmdline
+        self.newcmdline = newcmdline
         return newcmdline
-def upTodownTravelDir(rootDir,OperatorWithData,maxdepth=9999,curdepth=0,Interceptor=None):
+def upTodownTravelDir(rootDir, OperatorWithData, datadepth=9999, curdepth=0, mode2_Interceptor=[]):
     """
         Interceptor=([subdir names list],depth of the names expected)
     """
     print(rootDir)
-    if maxdepth==0:
-        newcmdline=OperatorWithData.process(rootDir,curdepth)
-        print(rootDir,newcmdline)
+    if datadepth == 0:
+        # data files are under the curdepth
+        newcmdline = OperatorWithData.process(rootDir, curdepth)
+        print('rootDir', rootDir, newcmdline)
         return
     # now go into a deeper dir
-    curdepth=curdepth+1
-    maxdepth=maxdepth-1
+    curdepth = curdepth + 1
+    datadepth = datadepth - 1
     for elem in os.listdir(path=rootDir):
-        path = rootDir+"/"+elem
+        path = rootDir + "/" + elem
         if not os.path.isdir(path):
-            #this is a data file
+            # this is a data file
             pass
-            #print("data",path)
+            # print("data",path)
         else:
-            #this is a folder
-            if Interceptor!=None and curdepth==Interceptor[1] and elem not in Interceptor[0]:
-                continue 
-            print("go into folder",path)
-            upTodownTravelDir(path,OperatorWithData,maxdepth,curdepth)
+            # this is a folder
+            if len(mode2_Interceptor) > 1 and curdepth == mode2_Interceptor[0] and elem not in mode2_Interceptor[1:]:
+                continue  # this is for mode 2 only
+            print("go into folder", path)
+            upTodownTravelDir(path, OperatorWithData, datadepth, curdepth)
--- src/pipelinecontrol/scriptcontrol.py
+++ src/pipelinecontrol/scriptcontrol.py
@@ -7,8 +7,7 @@ Created on 2014-11-7
 from optparse import OptionParser
 import re, os
 
-from pipelinecontrol.Util import OperatorWithData_mode1, upTodownTravelDir, \
-    OperatorWithData_mode2
+from src.pipelinecontrol.Util import OperatorWithData_mode1,upTodownTravelDir,OperatorWithData_mode2
 
 
 parser = OptionParser()
@@ -16,14 +15,14 @@ parser = OptionParser()
 parser.add_option("-p", "--inputdatapath", dest="inputdatapath", help="output data name is defined as 'inputdatapath folder name'+'is subfolder name'+'is subfolder name'+...")
 parser.add_option("-c", "--cmdexample", dest="cmdexamplefile",help="oneline scriptexamplefile")
 parser.add_option("-o", "--outputpath", dest="outputpath", help="outputpath")
-parser.add_option("-d", "--datadepth", dest="datadepth", help="data need to be process in the depth from the inputdatapath,the depth of the inputdatapath is 0")
+parser.add_option("-d", "--datadepth", dest="datadepth", help="it's the depth of the dir from the inputdatapath which the data file that need to be process in it,the depth of the inputdatapath is 0")
 
 
-parser.add_option("-s", "--suffix", dest="suffixname", default="", help=".bam .bai .sam .vcf .blast and so on. note this is just used in the cmdline output parameter")
+parser.add_option("-s", "--suffix", dest="suffixname", default="", help="bam bai sam sorted.bam vcf blast and so on. note this is just used in the cmdline output parameter")
 parser.add_option("-m", "--mode", dest="mode",
                   help="1 :means produce cmdline scripts for every terminal folder,the input data should be all the data files under the terminal folder. 2:use all selected data files as the input parameters in the only one cmdline script")
-# parser.add_option("-w","--winWidth",dest="winWidth",default="40000",help="win width ")
-# parser.add_option("-X", "--winType", dest="winType", default="zvalue", help="winvalue or zvalue")
+parser.add_option("-1","--depthoffoldertocopy",dest="depthoffoldertocopy",default="0",help="0 means don't creat folder in the output folder")
+parser.add_option("-2", "--interceptdirs", dest="interceptdirs", default=[],action="append", help="winvalue or zvalue")
 parser.add_option("-q", "--quiet",
                   action="store_false", dest="verbose", default=True,
                   help="don't print status messages to stdout")
@@ -34,68 +33,39 @@ inputdatapath=options.inputdatapath
 
 outputpath=options.outputpath
 outsuffix=options.suffixname
-mode=options.mode
+mode=int(options.mode)
+n_subdirs=int(options.depthoffoldertocopy)
 
 cmdline=open(options.cmdexamplefile,'r').readline()
-print(cmdline)
-subtargets=re.findall(r"\${.*?}",cmdline)
-outputidx=subtargets.index("${output}")
 
-dataInDirMap={}
-targetdatasuffix=[]
+mode2_Interceptor=options.interceptdirs
+print(mode2_Interceptor)
 
-for target in subtargets:
-    c=re.search(r'\${(.*?)}',target).group(1)
-    if "output"!=c:
-        targetdatasuffix.append(c)
-        
-        
-print("targetdatasuffix",targetdatasuffix)
-
-
-operatorwithdata_mode1=OperatorWithData_mode1(cmdline)
-operatorwithdata_mode2=OperatorWithData_mode2(cmdline)
 if __name__ == '__main__':
     if mode==1:
-        upTodownTravelDir(inputdatapath,OperatorWithData=operatorwithdata_mode1,maxdepth=datadepth)
+        #friendly ui interaction
+        if options.depthoffoldertocopy==0:
+            print("option -1 default 0")
+        if len(mode2_Interceptor)>1:
+            print("warning: option -2 is useless in mode 1")
+        #progamma logic
+        operatorwithdata_mode1=OperatorWithData_mode1(cmdline,outputpath,outsuffix,inputdatapath,n_subdirs=n_subdirs)
+        upTodownTravelDir(inputdatapath,OperatorWithData=operatorwithdata_mode1,datadepth=datadepth)
         
     elif mode==2:
-        upTodownTravelDir(inputdatapath,OperatorWithData=operatorwithdata_mode2,maxdepth=datadepth)
-        finalcmdline=re.sub(r"-\wd+\s+\${.*?}"," ",operatorwithdata_mode2.cmdline)
+        if options.depthoffoldertocopy!=0:
+            print("warning: option -1 is not used in mode 2")
+            
+        operatorwithdata_mode2=OperatorWithData_mode2(cmdline,outputpath,outsuffix)
+        upTodownTravelDir(inputdatapath,OperatorWithData=operatorwithdata_mode2,datadepth=datadepth)
+        #finalcmdline=re.sub(r"\${output}")
+        finalcmdline=re.sub(r"[-\w\d]+[=\s]+\${.*?}"," ",operatorwithdata_mode2.newcmdline)
+        print(finalcmdline,file=open("F:/work/pipelinecontrol/scripts/"+outsuffix+"_script.sh",'a'))
     print("==============")
-    cmdline=operatorwithdata_mode1.cmdline
-#     popdirs=os.listdir(path=inputdatapath)
-#     for a in popdirs:
-#         if datadepth==3 and os.path.isdir(inputdatapath+"/"+a):
-#             dataInDirMap[inputdatapath+"/"+a]={}
-#             indvddirs=os.listdir(path=inputdatapath+"/"+a)
-#             for b in indvddirs:
-#                 if os.path.isdir(inputdatapath+"/"+a+"/"+b)==False:
-#                     print(b+" :shouldn't accour here")
-#                     continue
-#                 dataInDirMap[inputdatapath+"/"+a][b]=[]
-#                 datafiles=os.listdir(path=inputdatapath+"/"+a+"/"+b)
-#                 print(inputdatapath+"/"+a+"/"+b,"datafiles",datafiles) 
-#                 #ordered by the ${1} ${2} ... in cmdline
-#                 for i in range(0,len(targetdatasuffix)):
-#                     if targetdatasuffix[i]=="output":
-#                         continue
-#                     for datafilename in datafiles:
-#                         #print("targetdatasuffix ",str(i),targetdatasuffix[i],datafilename,subtargets[i],re.search(r".*?"+targetdatasuffix[i],datafilename),".*?"+targetdatasuffix[i])
-#                         if re.search(r".*?"+targetdatasuffix[i],datafilename)!=None:
-#                             # sub was acted from the first to the rear most
-#                             cmdline=re.sub(r"\${\s*"+targetdatasuffix[i]+"\s*}"," "+datafilename+" ",cmdline)
-#         elif datadepth==2 and os.path.isdir(inputdatapath+"/"+a):
-#             pass
-#         elif datadepth==1:
-#             datafiles=os.listdir(path=inputdatapath)
-#             for datafilename in datafiles:
-#                 if re.search(r".*?"+targetdatasuffix[0],datafilename)!=None:
-#                     cmdline=re.sub(r"\${\s*"+targetdatasuffix+"\s*}"," "+datafilename+" ",cmdline)
-#                     print(cmdline)
-                    
+#     cmdline=operatorwithdata_mode1.cmdline
+
                 
-    print(cmdline)
-    print(dataInDirMap)        
+    #print(cmdline,finalcmdline)
+       
 
 
