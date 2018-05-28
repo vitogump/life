8079260317a8e0c2fe6ae8d528f8bcccdf4687ac
vitogump <vitogump@163.com>
2018-05-28 17:26:02 +0800

    simplify

--- src/compass/chip_design.py
+++ src/compass/chip_design.py
@@ -8,81 +8,20 @@ import os,re,sys
 SNPIDmissedinextract=[]
 def extarctBlastOutvariablelenwaper(a):
     extarctBlastOutvariablelen(**a)
-def extarctBlastOutvariablelen(BlastOutFile,faMap,fromposOfBlastf="missedinextracttemp2"):
+def extarctBlastOutvariablelen(BlastOutFile,faMap,fromposOfBlastf=0):
     global SNPIDmissedinextract
-    posfile=open(BlastOutFile+".pos",'a')
 #     me=open(missedinextract,'r')
     ospid=os.getpid()
     f=open(BlastOutFile+"missedinextract"+str(ospid)+".fa",'a')
-    bo=open(BlastOutFile,"r")
-    bo.seek(fromposOfBlastf[0])
-    for outline in bo:
-        for snpid in SNPIDmissedinextract:
-            if snpid in outline.strip() and outline[:len(snpid.strip())]==snpid.strip():
-                hitlist = re.split(r"\s+", outline)
-                if int(hitlist[3])>=len(faMap[IDline.strip()])-10 and int(hitlist[4])<=3 and lastsnpID!=hitlist[0]:
-                    sendpos = int(hitlist[9])
-                    sstartpos = int(hitlist[8])
-                    qstartpos = int(hitlist[6])
-                    blastlen=int(hitlist[3])
-                    flanklen=faMap[IDline.strip()].index("N")
-                    snp_loc_s=sstartpos+flanklen
-                    snpindex = flanklen+1 - qstartpos
-                    if sstartpos > sendpos:
-                        temp = sstartpos
-                        sstartpos = sendpos
-                        sendpos = temp
-                        revcom="-"
-                    lastsnpID = hitlist[0]
-                    chrom= hitlist[1]
-                    hitlist = re.split(r"\s+", a.readline().strip())
-                    print(lastsnpID,chrom,snp_loc_s,file=posfile)
-                    break
-                else:#same snpID means not the first record, or not blast records (just #query)
-                    print(">"+IDline+faMap[IDline.strip()],file=f)
-                    sys.stdout.flush()
-            elif snpid in outline.strip():#
-                print(">"+IDline+faMap[IDline.strip()],file=f)
-                break#
-        if IDline.strip() in 
-           
-    for IDline in me:
-        
-            
-                
-                
-        a=os.popen("grep "+IDline.strip()+" "+BlastOutFile+"|awk '$1!~/^#/ && $4>="+str(len(faMap[IDline.strip()])-10)+" && $5<=3  {print $0}' " )
-        hit = a.readline()
-        if hit.strip()=="":
-            
-            continue
-        hitlist = re.split(r"\s+", hit)
-        sendpos = int(hitlist[9])
-        sstartpos = int(hitlist[8])
-        qstartpos = int(hitlist[6])
-        blastlen=int(hitlist[3])
-        flanklen=faMap[IDline.strip()].index("N")
-        snp_loc_s=sstartpos+flanklen
-        snpindex = flanklen+1 - qstartpos
-        if sstartpos > sendpos:
-            temp = sstartpos
-            sstartpos = sendpos
-            sendpos = temp
-            revcom="-"
-        lastsnpID = hitlist[0]
-        chrom= hitlist[1]
-        hitlist = re.split(r"\s+", a.readline().strip())
-        if lastsnpID!=hitlist[0]:
-            print(lastsnpID,chrom,snp_loc_s,file=posfile)
-        else:
-            print(">"+IDline+faMap[IDline.strip()],file=f)
-        sys.stdout.flush()
-    posfile.close()
-    me.close()
+
+    for snpid in SNPIDmissedinextract:
+        print(">"+snpid+"\n"+faMap[snpid],file=f)
+
     f.close()
-def extarctBlastOut(BlastOutFile,queryFaFile,flanklen=60,mismatch=6):
+        
+def extarctBlastOut(BlastOutFile,queryFaFile,filterlen=100,mismatch=6):
     print(" query id, subject id, % identity, alignment length, mismatches, gap opens, q. start, q. end, s. start, s. end, evalue, bit score")
-    ambigousfile=open(BlastOutFile+".ambigous.fa",'w')
+    ambigousfile=open(BlastOutFile+".ambigous.blastrec",'w')
     qfa=open(queryFaFile,'r')
     faMap={}
     subid=os.getpid()
@@ -95,7 +34,7 @@ def extarctBlastOut(BlastOutFile,queryFaFile,flanklen=60,mismatch=6):
     for ID in faMap.keys():
         print(ID,file=allIDf)
     allIDf.close()
-    a = os.popen("awk '$1!~/^#/ && $4>="+str(2*flanklen+1)+" && $5<="+str(mismatch)+"  {print $0}' " + BlastOutFile)
+    a = os.popen("awk '$1!~/^#/ && $4>="+str(filterlen)+" && $5<="+str(mismatch)+"  {print $0}' " + BlastOutFile)
     posfile=open(BlastOutFile+".pos",'w')
 
     revcom="+"
@@ -108,14 +47,16 @@ def extarctBlastOut(BlastOutFile,queryFaFile,flanklen=60,mismatch=6):
     qstartpos = int(hitlist[6])
     blastlen=int(hitlist[3])
     
-    snpindex = flanklen+1 - qstartpos
+    
     if sstartpos > sendpos:
         temp = sstartpos
         sstartpos = sendpos
         sendpos = temp
         revcom="-"
-    snp_loc_s=sstartpos+flanklen
+    
     lastsnpID = hitlist[0]
+    flanklen=faMap[lastsnpID.strip()].index("N")
+    snp_loc_s=sstartpos+flanklen
     chrom= hitlist[1]
 
     for hit in a:
@@ -130,8 +71,9 @@ def extarctBlastOut(BlastOutFile,queryFaFile,flanklen=60,mismatch=6):
             sendpos = int(hitlist[9])
             qstartpos = int(hitlist[6])
             blastlen=int(hitlist[3])
+            lastsnpID = hitlist[0]
+            flanklen=faMap[lastsnpID.strip()].index("N")
             snp_loc_s=sstartpos+flanklen
-            snpindex = flanklen+1 - qstartpos
             if sstartpos > sendpos:
                 temp = sstartpos
                 sstartpos = sendpos
@@ -139,7 +81,7 @@ def extarctBlastOut(BlastOutFile,queryFaFile,flanklen=60,mismatch=6):
                 revcom="-"
             else:
                 revcom="+"
-            lastsnpID = hitlist[0]
+            
     posfile.close()
     print("postion writing done")
     
@@ -150,16 +92,22 @@ def extarctBlastOut(BlastOutFile,queryFaFile,flanklen=60,mismatch=6):
 #     a = os.popen("less missedinextractmp"+str(subid)+"|wc -l ")
 #     totalsnpforcount=int(a.readline().strip())
 #     a.close()
+    f = open("missedinextractmp"+str(subid),'r')
+    for line in f:
+        SNPIDmissedinextract.append(line.strip())
+    f.close()
     parameterstuples_list=[]
+    f=open(BlastOutFile,'r')
     blastoutfilendpos=f.seek(0,os.SEEK_END)
-    d=int(blastoutfilendpos/36)+1
+    f.close()
+    d=int(blastoutfilendpos/1)+1
     j=0
     for i in range(0,blastoutfilendpos,d):
 #         print("sed -n '"+str(i)+","+str(i+d-1)+"p' missedinextractmp"+str(subid)+" >missedinextractmp"+str(subid)+str(j))
 #         os.system("sed -n '"+str(i)+","+str(i+d-1)+"p' missedinextractmp"+str(subid)+" >missedinextractmp"+str(subid)+str(j))
         parameterstuples_list.append({"BlastOutFile":BlastOutFile,"faMap":faMap,"fromposOfBlastf":i})
         j+=1
-    pool=Pool(36)
+    pool=Pool(1)
     pool.map(extarctBlastOutvariablelenwaper,parameterstuples_list)
     pool.close()
     pool.join()
