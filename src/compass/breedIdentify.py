ad9bf2b1892ffe6ec9a290e12d99a9dbe828917c
vitogump <vitogump@163.com>
2018-08-20 18:18:13 +0800

    blind test tansheep update lib

--- src/compass/breedIdentify.py
+++ src/compass/breedIdentify.py
@@ -11,11 +11,12 @@ parser = OptionParser()
 parser.add_option("-l", "--seqlib", dest="seqlib",default=None,# action="callback",type="string",callback=useoptionvalue_previous2,
                   help="first col corresponding vcf's, second corresponding new. all vcf's chrom is not necessary. transchr only occur in outfile")
 parser.add_option("-i","--indgenotypefile",dest="indgenotype",help="default infile1_infile2")
-
+parser.add_option("-u","--update",dest="identifiedresultoupdatelib",nargs=2,default=None,help="if this option exist, then updatelib, otherwise identify samples")
 parser.add_option("-o", "--output", dest="output", default=True,
                   help="don't print status messages to stdout")
 (options, args) = parser.parse_args()
 
+No_ofsites=18
 DIPLOTYPES = ['A', 'C', 'G', 'K',"k","m", 'M', 'N', 'S',"s","r", 'R', 'T', 'W',"w","y", 'Y']
 PAIRS = ['AA', 'CC', 'GG', 'GT',"TG","CA", 'AC', 'NN', 'CG',"GC","GA",'AG', 'TT', 'AT',"TA",'TC','CT']
 diploHaploDict = dict(zip(DIPLOTYPES,PAIRS))
@@ -34,7 +35,7 @@ judgeIndlist=[]
 if __name__ == '__main__':
     #readlib
     libf=open(options.seqlib,'r')
-    print(libf.readline())
+    libtitle=libf.readline()
     for seqcline in libf:
         linelist=re.split(r"\s+",seqcline.strip())
         tetramerPos=tuple(re.split(r"\+",linelist[0]))
@@ -48,10 +49,15 @@ if __name__ == '__main__':
         else:
             seqlibMapBy4Pos[tetramerPos]=[tetramerSeq]
             NumlibMapBy4Pos[tetramerPos]=[(int(linelist[2]),int(linelist[3]))]
-    print(len(seqlibMapBy4Pos),seqlibMapBy4Pos,sep="\n")
+    print(len(seqlibMapBy4Pos),*seqlibMapBy4Pos,sep="\n")
+    for pos1,pos2,pos3,pos4 in seqlibMapBy4Pos.keys():
+        print(pos1,pos2,pos3,pos4)
+        print(seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)])
+        print(NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)])
     libf.close()
-    
+#     exit()
     toBeTestindf=open(options.indgenotype,'r')
+    print(options.indgenotype)
     titleline=toBeTestindf.readline()
     #read tobetest ind info
     for pos in re.split(r"\t",titleline.strip())[4:]: positionlist.append(pos.strip())
@@ -66,19 +72,53 @@ if __name__ == '__main__':
                 seq.append("N")
             else:
                 seq.append(haploDiploDict[g.strip().upper()])
-        seq+=["N"]*(18-len(seq))
+        seq+=["N"]*(No_ofsites-len(seq))
         genotypeOfeachInd.append(seq)
     print(*genotypeOfeachInd,sep="\n")
     print(len(genotypeOfeachInd),len(positionlist))
+    if options.identifiedresultoupdatelib!=None:
+        answerfile=open(options.identifiedresultoupdatelib[0],'r')
+        answertitle=answerfile.readline()
+        for ans in answerfile:
+            anslist=re.split(r"\s+",ans.strip())
+            if anslist[0].strip() in indnamelist:
+                indidx=indnamelist.index(anslist[0].strip())#used to find genotype
+                for pos1,pos2,pos3,pos4 in seqlibMapBy4Pos.keys():
+                    pos1idx,pos2idx,pos3idx,pos4idx=positionlist.index(pos1),positionlist.index(pos2),positionlist.index(pos3),positionlist.index(pos4)
+                    indseq=genotypeOfeachInd[indidx][pos1idx]+genotypeOfeachInd[indidx][pos2idx]+genotypeOfeachInd[indidx][pos3idx]+genotypeOfeachInd[indidx][pos4idx]
+                    if indseq.upper() in seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)]:
+                        i=seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())
+                        NoOfT,NoOfNT=NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][i]
+                        if anslist[int(options.identifiedresultoupdatelib[1])-1].lower()=="y":
+                            NoOfT+=1
+                        elif anslist[int(options.identifiedresultoupdatelib[1])-1].lower()=="n":
+                            NoOfNT+=1
+                        NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][i]=(NoOfT,NoOfNT)
+                        print("update",anslist[0].strip())
+                    elif "N" not in indseq.upper():
+                        seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].append(indseq.upper())
+                        if anslist[int(options.identifiedresultoupdatelib[1])-1].lower()=="y":
+                            NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)].append((1,0))
+                        elif anslist[int(options.identifiedresultoupdatelib[1])-1].lower()=="n":
+                            NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)].append((0,1))
+        print("writing new lib...")
+        nlibf=open(options.output+"lib",'w')
+        print(libtitle.strip(),file=nlibf)
+        for pos1,pos2,pos3,pos4 in seqlibMapBy4Pos.keys():
+            for fourPosBase in seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)]:
+                print(pos1,pos2,pos3,pos4,sep="+",end="\t",file=nlibf)
+                print(diploHaploDict[fourPosBase[0]],diploHaploDict[fourPosBase[1]],diploHaploDict[fourPosBase[2]],diploHaploDict[fourPosBase[3]],sep="_",end="\t",file=nlibf)
+                print(*NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(fourPosBase)],sep="\t",file=nlibf)
+        nlibf.close()
+        exit()
     #start test
     for indidx in reversed(range(len(indnamelist))):
-        print(indidx)
         for pos1,pos2,pos3,pos4 in seqlibMapBy4Pos.keys():#each recombination
             pos1idx,pos2idx,pos3idx,pos4idx=positionlist.index(pos1),positionlist.index(pos2),positionlist.index(pos3),positionlist.index(pos4)
             indseq=genotypeOfeachInd[indidx][pos1idx]+genotypeOfeachInd[indidx][pos2idx]+genotypeOfeachInd[indidx][pos3idx]+genotypeOfeachInd[indidx][pos4idx]
             if indseq.upper() in seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)] and sum(NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())])>=10 and 0 in NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())]:
                 print(NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())])
-                print(indnamelist[indidx],tft[NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())].index(0)],"high confidence",file=fo)
+                print(indnamelist[indidx],tft[NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())].index(0)],"highconfidence",sep="\t",file=fo)
                 indnamelist.pop(indidx);genotypeOfeachInd.pop(indidx)
                 break
             elif indseq.upper() in seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)]:
@@ -94,14 +134,14 @@ if __name__ == '__main__':
             if indseq.upper() in seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)] :
                 print(NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())])
                 if NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())][0]>NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())][1]:
-                    print(indnamelist[indidx],"滩羊","low confidence",file=fo)
+                    print(indnamelist[indidx],"滩羊","lowconfidence",sep="\t",file=fo)
                 else:
-                    print(indnamelist[indidx],"非滩羊","low confidence",file=fo)
+                    print(indnamelist[indidx],"非滩羊","lowconfidence",sep="\t",file=fo)
 #                 print((pos1,pos2,pos3,pos4),NumlibMapBy4Pos[(pos1,pos2,pos3,pos4)][seqlibMapBy4Pos[(pos1,pos2,pos3,pos4)].index(indseq.upper())],indidx)
                 indnamelist.pop(indidx);genotypeOfeachInd.pop(indidx)
                 break
         else:
-            print(indnamelist[indidx],"非滩羊\tverylowconfidence",file=fo)
+            print(indnamelist[indidx],"非滩羊\tverylowconfidence",sep="\t",file=fo)
     print(indnamelist,len(indnamelist))
     toBeTestindf.close();fo.close()
 #             print(pos1,pos2,pos3,pos4)
