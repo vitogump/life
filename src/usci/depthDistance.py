86e7d34e62b8496242bddf47db1050ec1f368f17
ruiliu <vitogump@163.com>
2019-08-02 14:33:16 +0800

    three dist array were properly produced, mds could be calculate in R

--- src/usci/depthDistance.py
+++ src/usci/depthDistance.py
@@ -10,12 +10,14 @@ import os,re
 import pandas as pd
 import numpy as np
 from scipy.interpolate import UnivariateSpline as spline
+from scipy.spatial.distance import pdist
 
 parser = OptionParser()
 parser.add_option("-d", "--flexDir", dest="flexDir", help="")
+parser.add_option("-t", "--distype", dest="distype",default=None, help="")
 parser.add_option("-s", "--selectbinfiles", dest="selectbinfiles")
 
-parser.add_option("-o", "--output", dest="output")
+parser.add_option("-o", "--outputprefix", dest="outputpre")
 
 (options, args) = parser.parse_args()
 flexDir=options.flexDir
@@ -43,8 +45,11 @@ def cosine_distance(matrix1,matrix2):
     cosine_distance = np.divide(matrix1_matrix2, np.dot(matrix1_norm, matrix2_norm.transpose()))
     return cosine_distance
 
-mds=[["0" for i in range(75)] for j in range(75)]
-of=open(options.output,"w")
+mds_scale=[["0" for i in range(75)] for j in range(75)]
+mds_correct=[["0" for i in range(75)] for j in range(75)]
+
+ofs=open(options.outputpre+str(options.distype)+"scaledDepth","w")
+ofc=open(options.outputpre+str(options.distype)+"GCcorrectedDepth","w")
 if __name__ == '__main__':
     #read file
     for elem in os.listdir(path=flexDir):
@@ -70,12 +75,12 @@ if __name__ == '__main__':
     #correct the depth by GC 
         GCord_idx=list(fpdfiltered.loc[:,"GC"].sort_values().index)
         reGCord_idx=np.argsort(GCord_idx)
-        print(*fpdfiltered["depth_scale"].reindex(GCord_idx).values,sep="\n",file=open("temp_orddepth_scale",'w'))
-        print(*GCord_idx,file=open("GCord",'w'))
-        print(*reGCord_idx,sep="\n",file=open("temp_reord",'w'))
-        print("start spline")
+#         print(*fpdfiltered["depth_scale"].reindex(GCord_idx).values,sep="\n",file=open("temp_orddepth_scale",'w'))
+#         print(*GCord_idx,file=open("GCord",'w'))
+#         print(*reGCord_idx,sep="\n",file=open("temp_reord",'w'))
+#         print("start spline")
         result=spline(fpdfiltered.loc[:,"depth_scale"].index.values,fpdfiltered["depth_scale"].reindex(GCord_idx).values)
-        print("finish spline",fpdfiltered.index.values.max())
+#         print("finish spline",fpdfiltered.index.values.max())
 #         xint=np.linspace(fpdfiltered.index.values.min(),fpdfiltered.index.values.max(),1000)
         print(*result(fpdfiltered.index.values),sep="\n",file=open("temp_spline.txt","w"))
         fpdfiltered["depth_correct"]=fpdfiltered["depth_scale"].values-result(fpdfiltered.index.values)[reGCord_idx]
@@ -91,19 +96,39 @@ if __name__ == '__main__':
 
             
     #calculate distance & file distance array
-            a=flex_data1.loc[:,"depth_scale"].values;b=flex_data2.loc[:,"depth_scale"].values
-            dist=np.dot(a,b)/(np.linalg.norm(a)*np.linalg.norm(b))
-            mds[flex_idx2][flex_idx1]=mds[flex_idx1][flex_idx2]=str(1-dist)
+            a1=flex_data1.loc[:,"depth_scale"].values;b1=flex_data2.loc[:,"depth_scale"].values
+            a2=flex_data1.loc[:,"depth_correct"].values;b2=flex_data2.loc[:,"depth_correct"].values
             
+            if options.distype.lower()=="cos" or options.distype.lower()=="c":
+                dist1=1-np.dot(a1,b1)/(np.linalg.norm(a1)*np.linalg.norm(b1))
+                dist2=1-np.dot(a2,b2)/(np.linalg.norm(a2)*np.linalg.norm(b2))
+            elif options.distype.lower()=="euclideanDistances" or options.distype.lower()=="e":
+                dist1=np.linalg.norm(a1-b1)
+                dist2=np.linalg.norm(a2-b2)
+            elif options.distype.lower()=="hellinger" or options.distype.lower()=="h":
+                dist1=1/np.sqrt(2)*np.linalg.norm(np.sqrt(a1)-np.sqrt(b1))
+                dist2=1/np.sqrt(2)*np.linalg.norm(np.sqrt(a2)-np.sqrt(b2))
+                print(a1,b1,dist1,a2,b2,dist2)
+            mds_scale[flex_idx2][flex_idx1]=mds_scale[flex_idx1][flex_idx2]=str(dist1)
+            mds_correct[flex_idx2][flex_idx1]=mds_correct[flex_idx1][flex_idx2]=str(dist2)
 #     print("\nmds",mds)
     
-    print("",end="\t",file=of)
+    print("",end="\t",file=ofs)
     for fname in flexfilelist:
-        print(re.search(r"[^/]*$",fname).group(0),end="\t",file=of)
-    print("",file=of)
+        print(re.search(r"[^/]*$",fname).group(0),end="\t",file=ofs)
+    print("",file=ofs)
     n_i=0
-    for distlist in mds:
-        print(re.search(r"[^/]*$",flexfilelist[n_i]).group(0),"\t".join(distlist),sep="\t",file=of)
+    for distlist in mds_scale:
+        print(re.search(r"[^/]*$",flexfilelist[n_i]).group(0),"\t".join(distlist),sep="\t",file=ofs)
         n_i+=1
-    of.close()
+    ofs.close()
+    print("",end="\t",file=ofc)
+    for fname in flexfilelist:
+        print(re.search(r"[^/]*$",fname).group(0),end="\t",file=ofc)
+    print("",file=ofc)
+    n_i=0
+    for distlist in mds_correct:
+        print(re.search(r"[^/]*$",flexfilelist[n_i]).group(0),"\t".join(distlist),sep="\t",file=ofc)
+        n_i+=1
+    ofc.close()
     #
