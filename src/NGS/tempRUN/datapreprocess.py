ee03fd4deceaefa90dac11532155d79fc29b431e
ruiliu <vitogump@163.com>
2019-08-12 18:08:22 +0800

    go to NIPT GC issues

--- src/NGS/tempRUN/datapreprocess.py
+++ src/NGS/tempRUN/datapreprocess.py
@@ -8,27 +8,35 @@ import os,re
 # from NGS.Analysis.IntervalValueToBoxplot import path
 
 parser = OptionParser()
-parser.add_option("-f", "--configfile", dest="configfile",help="configfile ")
+parser.add_option("-d", "--rootdir", dest="rootdir",help="data files directly under this dir ")
+parser.add_option("-r", "--rmoriginalfile", dest="rmoriginalfile",action="store_true",default=False)
 parser.add_option("-s", "--suffix", dest="suffix",action="append",default=["1.fq.gz","2.fq.gz"],help="suffix for a folder to package")
+parser.add_option("-o", "--outdir", dest="outdir",default=None,help="outpath,default is the same fold as -f assigned ")
 (options, args) = parser.parse_args()
 
-cf=open(options.configfile,'r')
+# cf=open(options.rootdir,'r')
 
-filepath=cf.readline().strip()
+filepath=options.rootdir.strip()
+print(filepath)
+exit()
 files=os.listdir(filepath)
 folderMap={}#{"AS10_":[AS10_1.fq.gz,AS10_1.fq.gz]}
-os.system("cp "+os.path.join(filepath,"md5.txt")+" "+os.path.join(filepath,"md5.txt.copy"))
-
+# os.system("cp "+os.path.join(filepath,"md5*.txt")+" "+os.path.join(filepath,"md5*.txt.copy"))
+if options.outdir==None:
+    outputdir=filepath
+else:
+    outputdir=options.outdir.strip()
 if __name__ == '__main__':
     for fi in files:
-        l_pre=len(fi)-len(options.suffix[0])
+        
         fi_d=os.path.join(filepath,fi)
         if os.path.isdir(fi_d):
             pass
         else:
             for s in options.suffix:
                 if re.search(r"."+s,fi)!=None:
-                    data_d=os.path.join(filepath,fi[:l_pre])
+                    l_pre=len(fi)-len(s)
+                    data_d=os.path.join(outputdir,fi[:l_pre]).strip("_").strip("-")
                     if not os.path.exists(data_d):
                         os.makedirs(data_d)
                     os.system("cp "+fi_d+" "+data_d)
