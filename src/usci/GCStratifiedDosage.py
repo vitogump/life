623c464118dd4dedbe1c7ed8b47d1aa207a69b08
ruiliu <vitogump@163.com>
2019-08-13 17:07:28 +0800

    going to change to multiple threads

--- src/usci/GCStratifiedDosage.py
+++ src/usci/GCStratifiedDosage.py
@@ -17,33 +17,7 @@ parser.add_option("-m","--mindepth",dest="mindepth",help="mindepth for both arch
 parser.add_option("-s","--speciesesName",action="append",dest="speciesesName",default=[],help="speciesName in table")#
                                                                                                                                                           
 (options, args) = parser.parse_args()
-chromMap={
-    "chrM":16571,
-"chr1":249250621,
-"chr2":243199373,
-"chr3":198022430,
-"chr4":191154276,
-"chr5":180915260,
-"chr6":171115067,
-"chr7":159138663,
-"chr8":146364022,
-"chr9":141213431,
-"chr10":135534747,
-"chr11":135006516,
-"chr12":133851895,
-"chr13":115169878,
-"chr14":107349540,
-"chr15":102531392,
-"chr16":90354753,
-"chr17":81195210,
-"chr18":78077248,
-"chr19":59128983,
-"chr20":63025520,
-"chr21":48129895,
-"chr22":51304566,
-"chrX":155270560,
-"chrY":59373566}
-
+outfile=open(options.outfilepreName,'w')
 minvalue = float(options.interval[0])
 maxvalue = float(options.interval[1])
 breaks = int(options.interval[2])
@@ -79,6 +53,6 @@ if __name__ == '__main__':
         print(delta_DerAftotal)
         
     for a,b in sorted(delta_DerAftotal.keys()):
-        print(a,b,delta_DerAftotal[a,b],file=)
-    bamfile.close()
+        print(a,b,delta_DerAftotal[a,b],sep="\t",file=outfile)
+    bamfile.close();outfile.close()
         
