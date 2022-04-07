ef01766df7b361e4d22909b4040e9eb880bce0b0
vitogump <vitogump@163.com>
2022-04-08 01:04:03 +0800

    step 3, read sequence done; first step to find target convergent site done; go next 2

--- src/NGS/SwissArmyKnife/one2oneOrtholog.py
+++ src/NGS/SwissArmyKnife/one2oneOrtholog.py
@@ -3,12 +3,20 @@ Created on 2022年3月28日
 
 @author: RuiLiu
 '''
+"""
+Each function accepts either a file name or an open file handle, so data can be also loaded from compressed files, StringIO objects, and so on. 
+If the file name is passed as a string, the file is automatically closed when the function finishes; 
+"""
+
+from io import StringIO
 from optparse import OptionParser
 import os, re
+from os.path import isfile
 import pickle
 
+from Bio import AlignIO
+
 from NGS.BasicUtil import Util
-from os.path import isfile
 
 
 parser = OptionParser()
@@ -17,7 +25,7 @@ parser.add_option("-s", "--steps", dest="steps", help="1: selected assigend spec
 parser.add_option("-c", "--compliantFasta",dest="compliantFasta")
 parser.add_option("-m", "--musclepath",dest="musclepath")
 parser.add_option("-p", "--pamlpath",dest="pamlpath")
-parser.add_option("-n", "--speciesname", dest="speciesname",action="append",default=[], help="")
+parser.add_option("-t", "--targetSpeciesname", dest="targetSpeciesname",action="append",default=[], help="")
 (options, args) = parser.parse_args()
 if __name__ == '__main__':
     groupfile=open(options.groupfile,"r")
@@ -87,9 +95,11 @@ if __name__ == '__main__':
         for fn in fileHmap.keys():
             fileHmap[fn][0].close()
     elif options.steps=="3":
+        
         step2outpath=options.groupfile.replace(".","_")
         treefilefor14species="/home/lrui/specieslist14example_NAMED.nwk.txt"
         from Bio.Phylo.PAML import codeml
+        from Bio import Phylo,SeqIO
         cml=codeml.Codeml()
         "/home/lrui/paml4.9j/comdel.ctl"
         cml.read_ctl_file(os.path.join(options.pamlpath,"codeml.ctl"))
@@ -118,6 +128,75 @@ if __name__ == '__main__':
                 if stat!=0:
                     print("Error paml call");exit(-1)
                 #extract 
+                rst=open(os.path.join(step2outpath,fn.strip().replace("_align.fa.phy",".rst")),'r')
+                mlc=open(os.path.join(step2outpath,fn.strip().replace("_align.fa.phy",".mlc")),'r')
+                rstContent=rst.readlines();rstLine_idx=0
+                while rstLine_idx < len(rstContent):
+                    
+                    if "tree with node labels for Rod Page's TreeView" in rstContent[rstLine_idx]:
+                        while True:# for skip the empty line but It is not actually useful
+                            rstLine_idx+=1
+                            try:
+                                sf = StringIO(rstContent[rstLine_idx])
+                                treeWithNodeLables=Phylo.read(sf, "newick")#rstContent[rstLine_idx]
+                                tlist=[];sf.close()
+                                for targetSpeciesName in options.targetSpeciesname:
+                                    tlist.append(list(treeWithNodeLables.find_clades(name=r"\d*_"+targetSpeciesName))[0])
+                                break
+                            except:
+                                print("search Tree Failed? ",rstContent[rstLine_idx],"continue searching the tree")
+                                
+                        t_mrca = treeWithNodeLables.common_ancestor(*tlist)
+                    if "List of extant and reconstructed sequences" in rstContent[rstLine_idx]:
+                        sf=StringIO()
+                        sf.write(rstContent[rstLine_idx+2]);nodes_bases=re.split(r"\s+",rstContent[rstLine_idx+2].strip())# #nodes #bases
+                        rstLine_idx+=4;
+                        while re.search(r"[\w\W]{1,30}\s+([\w]{10}\s*)*",rstContent[rstLine_idx])!=None:
+                            sf.write(rstContent[rstLine_idx].strip()+"\n")
+                            rstLine_idx+=1
+                        sf.seek(0)
+                        """ no gap in align seq already"""
+                        aln_anc_seqgenerator=AlignIO.parse(sf,'phylip')#<class 'generator'>
+                        species_names=[];aaalignseqs=[]
+                        seq_rec=aln_anc_seqgenerator.__next__()
+                        if len(treeWithNodeLables.get_terminals()+treeWithNodeLables.get_nonterminals())!=len(seq_rec):
+                            print("Error somewhere,inconstant amount of tree not and seqs")
+                        print("loading ancestrall stat/info finished")
+                        #collect c==d
+                        cd_sites_idx=[]
+                        for i in range(int(nodes_bases[1])):
+                            pos_i_targetsA=[seqrecord_obj[i] for seqrecord_obj in seq_rec if seqrecord_obj.id in options.targetSpeciesname]
+                            if pos_i_targetsA.count(pos_i_targetsA[0])==len(options.targetSpeciesname):
+                                cd_sites_idx.append(i)
+                        #search changes along each target to root,A!=C,B!=D
+                        for i in cd_sites_idx:
+                            for t_clade in tlist:
+                                print()
+                            else:
+                                print("no stasifid,skip site:",i)
+                                continue
+                            print(treeWithNodeLables.trace(t_mrca,tlist[0]))
+                            
+
+
+#                             for seqrecord_obj in seq_rec.id:
+#                                 if seqrecord_obj['id'=
+#                         for seq_rec in aln_anc_seqgenerator:
+#                             
+#                                 
+#                             for 
+#                                 print(seqrecord_obj)
+#                                 print(type(seqrecord_obj),seqrecord_obj.id)
+#                                 print(seqrecord_obj.seq)
+#                         print(type(aln_anc_seqgenerator))
+
+
+                        
+                        print("ssss")
+                    rstLine_idx+=1        
+                #
+                t_mrca = treeWithNodeLables.common_ancestor(*tlist).confidence
+                  
 
     print("done")
     groupfile.close()
